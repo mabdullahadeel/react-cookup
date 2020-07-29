ee007d6e0b64cb7948c1a17a1e73c8de5fa5c6d7
Abdullah Sial <easyautsol@gmail.com>
2020-07-30 03:15:20 +0500

    Week 1 Exercise 1 React Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // importing react based components from bootstrap that are already made for reuse
 import { Navbar, NavbarBrand } from 'reactstrap';
@@ -6,18 +6,32 @@ import { Navbar, NavbarBrand } from 'reactstrap';
 // importing components that has been created
 import Menu from './components/MenuComponent';
 
-function App() {
-  return (
-    <div className="App">
-      {/* Adding the Navbar from the reactstrap based Bootstrap */}
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">My First React Application</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+// imorting dishes information from a separate file in the dihese folder
+import { DISHES } from './shared/dishes';
+import { render } from '@testing-library/react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {/* Adding the Navbar from the reactstrap based Bootstrap */}
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">My First React Application</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
+
 }
 
 export default App;
